97e8a4309fd3bbecf1d251b0ed664388d4f9234f
Veerwhitehat <92624033+Veerwhitehat@users.noreply.github.com>
2021-12-06 16:01:38 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function adduser(){
+    var player1=document.getElementById("player1").value;
+    var player2=document.getElementById("player2").value;
+
+    localStorage.setItem("player1",player1);
+    localStorage.setItem("player2",player2);
+
+    window.location="index2.html";
+    
+}

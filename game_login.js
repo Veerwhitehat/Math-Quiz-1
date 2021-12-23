bd3e177955175e6e1d3f236b7857a25cacaab987
Veerwhitehat <92624033+Veerwhitehat@users.noreply.github.com>
2021-12-23 19:10:12 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -9,13 +9,19 @@ function adduser(){
     
 }
 
+
 function send() {
-    number1 = document.getElementById("number1").value;
-    number2 = document.getElementById("number2").value;
+    number1 = document.getElementById(number1).value;
+    number2 = document.getElementById(number2).value;
+    localStorage.getItem(player1);
+    localStorage.getItem(player2);
+    document.getElementById("qturn").value;
+    document.getElementById("aturn").value;
+
     actual_answer = parseInt(number1) * parseInt(number2);
     question_number="<h4>" + number1 + "X" + number2 + "</h4>";
     input_box = "<br>Answer : <input type='text' id= 'input_check_box'>";
-    check_button = "<br><br><button class='btn btn-info' onclick='check'>Check</button>"
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>"
     row = question_number + input_box + check_button;
     document.getElementById("output").innerHTML=row;
     document.getElementById("number1").value="";
